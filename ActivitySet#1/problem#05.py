4b6553c1e17ddb7f90b8dbe87099cc7164c2458a
Srujana Reddy <93113277+SrujanaReddi@users.noreply.github.com>
2022-05-15 11:51:16 +0000

    .

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,10 +1,10 @@
 # Functions
 
 
-def computepay(h, r):
-    pass  # ...
+def computepay(hrs, rte):
+    pass 
 
-
+computepay=hrs*rte
 hrs = float(input("Enter hours? "))
 rte = float(input("Enter rate per hour? "))
 
