274c7765fbd74b9b18ffa7eb07d6d764932bfe61
Srujana Reddy <93113277+SrujanaReddi@users.noreply.github.com>
2022-05-18 03:30:27 +0000

    done

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,4 +1,9 @@
-hrs=float(input("Enter Hours"))
-rate=float(input("Enter Rate"))
-Earning=hrs*rate
-print("Pay:",Earning)
+# Conditional Execution
+
+hrs = float(input("Enter hours? "))
+rate = float(input("enter rate"))
+if hrs<=40:
+  pay = hrs*rate
+else:
+  pay = ((hrs-40)*rate*1.5)+ (40*rate)
+print(pay)
--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,9 +1,11 @@
-# Conditional Execution
-
-hrs = float(input("Enter hours? "))
-rate = float(input("enter rate"))
-if hrs<=40:
-  pay = hrs*rate
-else:
-  pay = ((hrs-40)*rate*1.5)+ (40*rate)
-print(pay)
+score = float(input("Enter the score"))
+if score >=0.9:
+ print("A")
+elif score >=0.8:
+ print("B")
+elif score >=0.7:
+ print("C")
+elif score >=0.6:
+ print ("D")
+elif score <0.6:
+ print("F")
